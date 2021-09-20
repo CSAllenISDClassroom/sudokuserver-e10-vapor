0fe8cb25fbe5f38212239d37541449fbd34947c8
Noah chin <william.chin@student.allensid.org>
2021-09-20 04:16:10 -0500

    board and boardID classes

---
+++ Sources/App/boardID.swift
@@ -0,0 +1,21 @@
+public class BoardID {
+
+    public var boardID : Dictionary<Int, Board> = [:]
+
+    var board = Board.init()
+  
+    
+    public func createBoardID(difficulty:String) {
+        board.createBoard(difficulty: difficulty)
+        let generateBoardID = Int.random(in: 0...9999)
+
+        for (key,_) in boardID {
+
+            if key != generateBoardID {
+                boardID[generateBoardID] = board
+            }
+        }
+
+    }
+    
+}
