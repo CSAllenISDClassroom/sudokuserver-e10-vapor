26416482624029f37b00577b249851fe1699cd54
Frank4lif3 <li.fan@student.allenisd.org>
2021-10-06 02:37:04 -0500

    syncing with kush

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,21 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    //array of boards
     var games : [Board] = []
     
     app.get { req in
         return "It works!"
     }
 
-    app.post("games") { req -> Response in
+    app.post("games", difficultyLevel: .Difficulty) { req -> Response in
         var difficultyLevel = Difficulty.medium
         do {
-            if let inputDifficulty 
+            guard let inputDifficulty = req.parameters.get("name")
+            else {
+                return Response(status:.badRequest)
+            }
+            difficultyLevel = inputDifficulty
+        }
+    }
 }
