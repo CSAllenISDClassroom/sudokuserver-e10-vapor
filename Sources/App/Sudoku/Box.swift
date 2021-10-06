8b51edc015d385fec202fd3faf7ab14a90e44aad
Elvin Li <elvin.li@student.allenisd.org>
2021-10-06 02:54:06 -0500

    Created Docstrings for the Classes of Box, Row, and Column

--- Sources/App/Sudoku/Box.swift
+++ Sources/App/Sudoku/Box.swift
@@ -1,3 +1,7 @@
 import Foundation
+
 public class Box : NineCells {
+    
+    // Class Box inherits the properties from parent class NineCells
+    
 }
--- Sources/App/Sudoku/Column.swift
+++ Sources/App/Sudoku/Column.swift
@@ -1,3 +1,7 @@
 import Foundation
+
 public class Column : NineCells {    
+
+    // Class Column inherits the properties from parent class NineCells
+
 }
