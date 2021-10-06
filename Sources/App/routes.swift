8f1917a8ca40dfb122de6cb1820463206daa3658
Kush Gupta <kush.gupta@student.allenisd.org>
2021-10-06 02:34:37 -0500

    Updates routes.swift with more get commands

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,12 +3,26 @@ import Vapor
 func routes(_ app: Application) throws {
     var games : [Board] = []
     
+    //////////////////////////////////////////////////
+    // GET commands are used to retrieve a resource //
+    //////////////////////////////////////////////////
+
     app.get { req in
         return "It works!"
+    }
+
+    // Running this GET Request will return a response with the supplied string
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
     }
 
     app.post("games") { req -> Response in
         var difficultyLevel = Difficulty.medium
         do {
             if let inputDifficulty 
+        }
+    }
+    
 }
+
