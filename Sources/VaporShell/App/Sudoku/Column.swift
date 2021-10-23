f19fd11b661894cf9bac26fac41f987991852ab8
Noah chin <william.chin@student.allensid.org>
2021-10-22 23:53:22 -0500

    Removed old files, Row, Column and Difficulty classes and enums

--- Sources/VaporShell/App/Sudoku/Column.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public class Column : NineCells {    
-
-    // Class Column inherits the properties from parent class NineCells
-
-}
--- Sources/VaporShell/App/Sudoku/Difficulty.swift
+++
@@ -1,6 +0,0 @@
-import Vapor
-import Foundation
-
-struct Difficulty: Content {
-    var difficulty: String?
-}
